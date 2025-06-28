ac2d7cfbacb4b990521b37222eaa434fa344128e
sezondo <psdd205050@gmail.com>
2025-06-28 20:22:33 +0900

    Re-add all files with LFS tracking

---
+++ program/T3/Assets/Data/Rob01Data.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable/RobData", fileName = "Rob01")]
+
+public class Rob01Data : ScriptableObject
+{
+    public int hp = 100;
+}
---
+++ program/T3/Assets/Script/RobHp.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class RobHp : MonoBehaviour
+{
+
+    public Rob01Data rob01Data;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
