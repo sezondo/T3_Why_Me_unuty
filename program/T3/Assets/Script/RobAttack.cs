6e69316163c1520a3347bdc7c7f2540aaf3e6eef
sezondo <psdd205050@gmail.com>
2025-07-03 17:30:54 +0900

    attack 구현

---
+++ program/T3/Assets/Script/RobAttack.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using System.Collections;
+
+public class RobAttack : MonoBehaviour
+{
+    private RobBase robBase;
+    private Animator animator;
+    private RobStooter[] shooter;
+    private bool CoroutineCheck;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        robBase = GetComponent<RobBase>();
+        shooter = GetComponentsInChildren<RobStooter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (robBase.currentState)
+        {
+            case UnitState.Idle:
+
+                break;
+
+            case UnitState.Attacking:
+
+                if (!CoroutineCheck)
+                {
+                    CoroutineCheck = true;
+                    StartCoroutine(Attacking());
+                }
+                break;
+
+            case UnitState.Moving:
+
+                break;
+
+            case UnitState.Dead:
+
+                break;
+        }
+    }
+
+    private IEnumerator Attacking()
+    {
+        while (true)
+        {
+            if (robBase.currentState != UnitState.Attacking)
+            {
+                break;
+            }
+
+            Fire();
+
+            animator.SetTrigger("Attack");
+
+            yield return new WaitForSeconds(robBase.data.attackSpeed);
+        }
+        CoroutineCheck = false;
+    }
+
+    private void Fire() {
+        foreach (var fp in shooter)
+        {
+            fp.Stoot();
+        }
+    }
+    
+
+
+}
